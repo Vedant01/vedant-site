f9dc89e7d3e2251130847bc5adb4d38d19a9d394
Vedant01 <vedant.agarwal312@gmail.com>
2025-04-24 14:38:14 +0530

    trae edits 3

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
       providerImportSource: '@mdx-js/react',
     }),
-    remixCloudflareDevProxy(),
+    process.env.NODE_ENV === 'development' ? remixCloudflareDevProxy() : null,
     remix({
       routes(defineRoutes) {
         return defineRoutes(route => {
@@ -34,5 +34,5 @@ export default defineConfig({
       },
     }),
     jsconfigPaths(),
-  ],
+  ].filter(Boolean),
 });
