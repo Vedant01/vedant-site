8861e29b9c9eb4cc9a1f70951e0969486abfe2bc
Vedant01 <vedant.agarwal312@gmail.com>
2025-04-24 16:23:39 +0530

    vercel customisation

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,80 +1,17 @@
 import { RemixServer } from '@remix-run/react';
+import { handleRequest } from '@vercel/remix';
 
-
-
-const ABORT_DELAY = 5000;
-
-// Helper to detect if we're in Vite dev (ESM) or Node.js prod (CommonJS)
-function canUsePipeableStream() {
-  try {
-    // Try to import the Node.js-only API
-    // This works in Node.js prod, fails in Vite dev
-    const ReactDOMServer = require('react-dom/server');
-    return typeof ReactDOMServer.renderToPipeableStream === 'function';
-  } catch {
-    return false;
-  }
-}
-
-export default async function handleRequest(
+export default function (
   request,
   responseStatusCode,
   responseHeaders,
   remixContext
 ) {
-  let PassThrough;
-  try {
-    PassThrough = (await import('stream')).PassThrough;
-  } catch {}
-  // Try Node.js streaming API first (prod)
-  let ReactDOMServer;
-  let usePipeableStream = false;
-  try {
-    ReactDOMServer = await import('react-dom/server');
-    usePipeableStream = typeof ReactDOMServer.renderToPipeableStream === 'function';
-  } catch {
-    ReactDOMServer = await import('react-dom/server');
-  }
-
-  if (usePipeableStream && PassThrough) {
-    // Node.js prod: use renderToPipeableStream
-    return new Promise((resolve, reject) => {
-      let didError = false;
-      const { pipe, abort } = ReactDOMServer.renderToPipeableStream(
-        <RemixServer context={remixContext} url={request.url} />,
-        {
-          onShellReady() {
-            const body = new PassThrough();
-            responseHeaders.set('Content-Type', 'text/html');
-            resolve(
-              new Response(body, {
-                status: didError ? 500 : responseStatusCode,
-                headers: responseHeaders,
-              })
-            );
-            pipe(body);
-          },
-          onShellError(error) {
-            reject(error);
-          },
-          onError(error) {
-            didError = true;
-            console.error(error);
-          },
-        }
-      );
-      setTimeout(abort, ABORT_DELAY);
-    });
-  } else {
-    // Vite dev: use renderToReadableStream (Web Streams API)
-    const { renderToReadableStream } = ReactDOMServer;
-    const stream = await renderToReadableStream(
-      <RemixServer context={remixContext} url={request.url} />
-    );
-    responseHeaders.set('Content-Type', 'text/html');
-    return new Response(stream, {
-      status: responseStatusCode,
-      headers: responseHeaders,
-    });
-  }
+  const remixServer = <RemixServer context={remixContext} url={request.url} />;
+  return handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixServer
+  );
 }
